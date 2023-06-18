dea46bb94ca4efb8ecd07218c484ba9c964abb77
Matt Clifford <matthewclifford@hotmail.co.uk>
2023-06-18 12:24:50 +0100

    update

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ6qtcSDucVgFKseT7PuQL9x3YzUnO9Cs",
+  authDomain: "emilysbloomingcakeco-e07db.firebaseapp.com",
+  projectId: "emilysbloomingcakeco-e07db",
+  storageBucket: "emilysbloomingcakeco-e07db.appspot.com",
+  messagingSenderId: "26616942924",
+  appId: "1:26616942924:web:9d2cff5e5e41d48f4e7fe9",
+  measurementId: "G-07Z3Q3MF4S"
+};
+
+const app = initializeApp(firebaseConfig);
